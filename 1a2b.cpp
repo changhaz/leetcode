c33ec956c6421df29fb0b2e8139a6db752c032a2
changhaz <zch1017@gmail.com>
2014-08-20 09:42:13 -0700

    pthread 1a2b

---
+++ 1a2b.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <pthread.h>
+
+using namespace std;
+
+int count = 0;
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER;
+pthread_cond_t condition_var = PTHREAD_COND_INITIALIZER;
+
+void* threadfunc1(void* v);
+void* threadfunc2(void* v);
+
+int main()
+{
+	pthread_t thread1;
+	pthread_t thread2;
+	
+	int result1 = pthread_create(&thread1,NULL,threadfunc1,NULL);
+	int result2 = pthread_create(&thread2,NULL,threadfunc2,NULL);
+	
+	pthread_join(thread1,NULL);
+	pthread_join(thread2,NULL);
+	return 0;
+}
+
+// 123
+void* threadfunc1(void* v)
+{
+	while (true) {
+		pthread_mutex_lock(&mutex1);
+		//cout<<"thread1 get lock"<<endl;
+
+		if (count%2 == 1) {
+			pthread_cond_wait(&condition_var,&mutex1);
+		}
+
+		cout<<" "<<count/2+1;
+		count++;
+		
+		pthread_mutex_unlock(&mutex1);
+
+		if (count>=5) {
+			return NULL;
+		}
+	}
+}
+
+// abc
+void* threadfunc2(void* v)
+{
+	while (true) {
+		pthread_mutex_lock(&mutex1);
+		//cout<<"thread2 get lock"<<endl;
+
+		if (count%2 == 1) {
+			cout<<" "<<char(count/2+'A');
+			count++;
+			pthread_cond_signal(&condition_var);
+		}
+		
+		pthread_mutex_unlock(&mutex1);
+		
+		if (count>=6) {
+			return NULL;
+		}
+		
+	}
+}
